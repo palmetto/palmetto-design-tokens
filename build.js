e84974825a576610a93cb868e33b20e935fe4303
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-06-10 16:57:45 -0400

    new build file to extend style dictionary

---
+++ build.js
@@ -0,0 +1,23 @@
+const StyleDictionary = require('style-dictionary');
+
+console.log('Build started...');
+console.log('\n==============================================');
+
+// Register Custom Filters
+StyleDictionary.registerFilter({
+  name: 'isColor',
+  matcher: (prop) => prop.attributes.category === 'color',
+});
+
+// APPLY THE CONFIGURATION
+// IMPORTANT: the registration of custom transforms
+// needs to be done _before_ applying the configuration
+StyleDictionaryExtended = StyleDictionary.extend('./config.json');
+
+
+// FINALLY, BUILD ALL THE PLATFORMS
+StyleDictionaryExtended.buildAllPlatforms();
+
+
+console.log('\n==============================================');
+console.log('\nBuild completed!');
