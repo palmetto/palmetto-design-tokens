4960433a68f1b1728f8c07419cf30d102b44cb60
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2020-12-03 09:29:21 -0800

    fix: only look for .svg files when generating icons (#103)
    * yarn
    
    * fix: only look for .svg files when generating icons
    
    Co-Authored-By: juanfabrega <19932356+juanfabrega@users.noreply.github.com>
    
    Co-authored-by: juanfabrega <19932356+juanfabrega@users.noreply.github.com>

--- utils/createIconComponents/createIconComponents.js
+++ utils/createIconComponents/createIconComponents.js
@@ -1,15 +1,14 @@
 const svgr = require('@svgr/core').default;
 const fs = require('fs');
 const path = require('path');
-const babel = require("@babel/core");
+const babel = require('@babel/core');
 const pascalCase = require('../pascalCase/pascalCase');
 const createFileHeader = require('../createFileHeader/createFileHeader');
 const indentLine = require('../indentLine/indentLine');
 
-const BABEL_OPTIONS = { plugins: [
-  '@babel/plugin-transform-react-jsx',
-  '@babel/plugin-transform-modules-commonjs',
-] };
+const BABEL_OPTIONS = {
+  plugins: ['@babel/plugin-transform-react-jsx', '@babel/plugin-transform-modules-commonjs'],
+};
 
 async function createIconComponents() {
   const icons = {};
@@ -17,9 +16,11 @@ async function createIconComponents() {
   const sourceIconsDir = path.join(__dirname, '..', '..', 'icons/');
   const buildIconsDir = path.join(__dirname, '..', '..', 'build', 'icons', 'react/');
 
-  const svgFiles = fs.readdirSync(sourceIconsDir);
+  const svgFiles = fs
+    .readdirSync(sourceIconsDir)
+    .filter(fileName => path.extname(fileName).toLowerCase() === '.svg');
 
-  svgFiles.forEach(function(filename) {
+  svgFiles.forEach(function (filename) {
     const fileContent = fs.readFileSync(sourceIconsDir + filename, 'utf-8');
     icons[filename.substr(0, filename.lastIndexOf('.'))] = fileContent;
   });
@@ -28,14 +29,14 @@ async function createIconComponents() {
     const iconName = Object.keys(icons)[i];
     const componentName = pascalCase(iconName);
 
-    const reactComponent = await svgr(icons[iconName], { icon: true }, { componentName })
+    const reactComponent = await svgr(icons[iconName], { icon: true }, { componentName });
 
     try {
       fs.readdirSync(buildIconsDir);
     } catch {
       fs.mkdirSync(buildIconsDir);
     }
-  
+
     const compiledComponent = babel.transformSync(reactComponent, BABEL_OPTIONS);
 
     fs.writeFileSync(buildIconsDir + `${componentName}.jsx`, compiledComponent.code);
@@ -45,12 +46,16 @@ async function createIconComponents() {
   let iconComponentsImports = '';
   let iconComponentsExport = `const icons = {\n`;
   iconComponentsIndexFile = createFileHeader(iconComponentsIndexFile);
-  
+
   for (let i = 0; i < Object.keys(icons).length; i++) {
     const iconName = Object.keys(icons)[i];
     const componentName = pascalCase(iconName);
-    iconComponentsImports = iconComponentsImports.concat(`import ${componentName} from './${componentName}';\n`);
-    iconComponentsExport = iconComponentsExport.concat(indentLine(`'${iconName}': ${componentName},\n`, 2));
+    iconComponentsImports = iconComponentsImports.concat(
+      `import ${componentName} from './${componentName}';\n`,
+    );
+    iconComponentsExport = iconComponentsExport.concat(
+      indentLine(`'${iconName}': ${componentName},\n`, 2),
+    );
   }
 
   iconComponentsExport = iconComponentsExport.concat('};\n\n export default icons;\n');
