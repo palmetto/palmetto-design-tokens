2bbeca7664f6748c688c10cbdce1924692846a44
Taylor Blanton <matthewtblanton@gmail.com>
2021-08-25 16:11:36 -0500

    Revert "fix: Have the figma token env variable message tell you to add the right env variable"
    This reverts commit 178c0bb3580c4a01495eda51b0776818210455ea.

--- utils/getFigmaDocument/getFigmaDocument.js
+++ utils/getFigmaDocument/getFigmaDocument.js
@@ -1,21 +1,21 @@
-const fetch = require("node-fetch");
-require("dotenv").config();
+const fetch = require('node-fetch');
+require('dotenv').config();
 
 if (!process.env.FIGMA_PERSONAL_ACCESS_TOKEN) {
   throw new Error(`Put your Figma token in a .env in the root directory of this directory.
   The .env file should look like this:
-  FIGMA_PERSONAL_ACCESS_TOKEN='xxxx'`);
+  PERSONAL_ACCESS_TOKEN='xxxx'`);
 }
 
 const requestConfig = {
-  method: "GET",
-  headers: { "x-figma-token": process.env.FIGMA_PERSONAL_ACCESS_TOKEN },
+  method: 'GET',
+  headers: { "x-figma-token": process.env.FIGMA_PERSONAL_ACCESS_TOKEN }
 };
 
-const requestUrl = "https://api.figma.com/v1/files/";
+const requestUrl = 'https://api.figma.com/v1/files/';
 
 const getFigmaDocument = (id, fileVersion) => {
-  const version = fileVersion ? `?version=${fileVersion}` : "";
+  const version = fileVersion ? `?version=${fileVersion}` : '';
 
   return fetch(`${requestUrl}${id}${version}`, requestConfig);
 };
