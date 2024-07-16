63f61ef4e6d30de79ac19011a2263cb9976be002
nathanyoung <1447339+nathanyoung@users.noreply.github.com>
2024-07-16 13:29:02 -0700

    fix: update to latest figma version

--- build.js
+++ build.js
@@ -103,7 +103,7 @@ const FIGMA_TOKENS_DOCUMENT = 'abGRptpr7iPaMsXdEPVm6W';
  * 3. The link will contain the version id as a querystring variable.
  * 4. Paste the version id here.
  */
-const FIGMA_FILE_VERSION = '6229118981';
+const FIGMA_FILE_VERSION = '6230562380';
 
 /**
  * Read tokens from FIGMA file.
